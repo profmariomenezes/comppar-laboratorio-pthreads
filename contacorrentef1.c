0bbf2e8f35cbd4447abcc859f6a996a826baaba7
Mario O. de Menezes <58959503+mariomack@users.noreply.github.com>
2026-02-27 18:21:08 -0300

    Create contacorrentef1.c

---
+++ contacorrentef1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main() {
+
+  
+
+  saldo = 1000.00;
+
+  pthread_create(..., depositos, ...); // a mesma funcao depositos() anterior
+
+  phtread_create(..., saques, ...); // a mesma funcao saques() anterior
+
+  printf("Saldo final: %.2lf\n", saldo);
+
+  return 0;  
+}
